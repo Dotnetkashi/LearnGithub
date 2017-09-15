fc2dff03844e5fc553bea9c2203e4d8e80f749d2
sahookashianth92@gmail.com <sahookashianth92@gmail.com>
2017-09-15 09:31:01 +0530

    Add the changes

---
+++ Testapp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+
+namespace Testapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = Directory.GetDirectories("/www/root");
+            Console.WriteLine("Null");
+            Console.WriteLine("Master");
+
+        }
+    }
+}
